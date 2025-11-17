daf29f554831ce4865c5fb9bb6c2df5af10c1585
Peter <braslavskiypv.ru@gmail.com>
2025-11-17 14:18:29 +0300

    newstart

--- src/index.js
+++ src/index.js
@@ -10,12 +10,72 @@ const client = new Client({
     ],
 });
 
-client.on('ready', (c) => {
+client.on('clientReady', (c) => {
     console.log(`${c.user.tag} is online`)
 })
 
 client.on('messageCreate', (message) => {
-    console.log(message.content);
+    console.log(message);
+    sendJson(message);
 })
 
 client.login(process.env.token);
+
+
+const http = require('http');
+const fs = require('fs');
+
+const optionsPost = {
+    hostname: 'localhost',
+    port: 8000,
+    path: '/server.php',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
+// Функция для отправки JSON
+const sendJson = (json) => {
+    const reqPost = http.request(optionsPost, (res) => {
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        res.on('end', () => {
+            console.log('Ответ от сервера (POST):', JSON.parse(data));
+        });
+    });
+
+    reqPost.on('error', (e) => {
+        console.error(`Проблема с запросом: ${e.message}`);
+    });
+
+    // Отправляем данные
+    reqPost.write(JSON.stringify(json));
+    reqPost.end();
+};
+
+// Чтение из файла или получение JSON из аргументов командной строки
+const input = "{\"content\": \"pong\"}";
+
+if (input) {
+    try {
+        // Если аргумент - это файл
+        if (fs.existsSync(input)) {
+            const jsonData = fs.readFileSync(input);
+            const jsonObject = JSON.parse(jsonData);
+            sendJson(jsonObject);
+        } else {
+            // Пытаться распарсить как JSON строку
+            const jsonObject = JSON.parse(input);
+            sendJson(jsonObject);
+        }
+    } catch (error) {
+        console.error('Ошибка при обработке JSON:', error.message);
+    }
+} else {
+    console.error('Пожалуйста, передайте JSON или путь к файлу JSON в качестве аргумента.');
+}
